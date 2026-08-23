8c80eb58a9ae799af2c092c76f5cb235baec00b2
DDD1305 <gabriel@deruda.fr>
2026-08-23 23:47:46 +0200

    Add createSocket, closeSocket, bindSocket, receiveMsg

---
+++ src/UDPcommunication/udp_socket.c
@@ -0,0 +1,89 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "udp_socket.h"
+
+/*
+int status; //socket indentification 
+
+void initCommunication(void){
+
+    struct addrinfo hints ={
+    .ai_family = AF_INET,
+    .ai_socktype = SOCK_DGRAM,
+    .ai_flags = AI_PASSIVE,
+    };
+}*/
+
+
+
+int createSocket(void){
+    int res = socket(AF_INET, SOCK_DGRAM, 0);
+    /*0 it's because we use default protocol dgram->UDP
+    * socket return a file descriptor
+    */
+    if(res == -1){
+        perror(NULL);//no prefix 
+        return -1;
+    }
+    return res;
+}
+
+
+int closeSocket(int socket){
+    int res =  close(socket);
+    if(res == -1){
+        perror(NULL);//no prefix 
+        return -1;
+    }
+    return res;
+}
+
+
+int bindSocket(int socket, const char* address, unsigned short int port){
+
+    struct sockaddr_in addr={0};
+
+    addr.sin_family= AF_INET;
+    addr.sin_port = htons(port);
+
+    int conversion = inet_pton(AF_INET, address, &addr.sin_addr);
+    if(conversion ==0){
+        printf("L'adresse fournie n'est pas IPV4");
+        return -1;
+    }else if (conversion ==-1){
+        perror(NULL);//no prefix 
+        return -1;
+    }
+
+    int res = bind(socket, ((struct sockaddr *) &addr), sizeof addr);
+    if(res == -1){
+        perror(NULL);//no prefix 
+        return -1;
+    }
+    return res;
+}
+
+
+int receiveMsg(int socket, char *buffer, int buffer_size){
+
+    printf("En attente d'un message...\n");
+    int res = recvfrom(socket, buffer, buffer_size-1, 0, NULL, NULL);
+    if(res == -1){
+        perror(NULL);
+        return -1;
+    }else if(res ==0){
+        return 0; 
+    }else if (res <= buffer_size-1){
+        buffer[res]='\0';
+        return res;
+    }else{
+        printf("The buffer size is too small ERROR");
+        return res;
+    }
+
+}
---
+++ src/UDPcommunication/udp_socket.h
@@ -0,0 +1,43 @@
+#ifndef UDP_SOCKET_H
+#define UDP_SOCKET_H
+
+
+/**
+ * @brief Creates an IPv4 UDP socket.
+ *
+ * @return The socket file descriptor, or -1 on error.
+ */
+int createSocket(void);
+
+
+/**
+ * @brief Closes a socket.
+ *
+ * @param socket_fd File descriptor of the socket to close.
+ * @return 0 on success, or -1 on error.
+ */
+int closeSocket(int socket);
+
+/**
+ * @brief Binds an IPv4 UDP socket to a local address and port.
+ *
+ * @param socket File descriptor of the socket.
+ * @param address IPv4 address written as text.
+ * @param port Local port in host byte order.
+ * @return 0 on success, or -1 on error.
+ */
+
+int bindSocket(int socket, const char* address, unsigned short int port);
+
+
+/**
+ *@brief wait the next msg send to socket and store it in the buffer and add \0 at the end
+ *
+ *@param socket file descriptor of the socket
+ *@param buffer the buffer that will store the message
+ *@param buffer_size his size
+ *@return -1 error 0 msg empty int>0 the number of bytes received
+ */
+
+int receiveMsg(int socket, char *buffer, int buffer_size);
+#endif
